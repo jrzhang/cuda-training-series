9a0a4c99bf10bbb880d4903c68ec8e95e9299f5c
jrzhang <johnrzhang@gmail.com>
2024-06-01 21:11:47 -0400

    Answers to part of hw1

--- exercises/hw1/hello.cu
+++ exercises/hw1/hello.cu
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+
 __global__ void hello(){
 
-  printf("Hello from block: %u, thread: %u\n", FIXME);
+  printf("Hello from block: %u, thread: %u\n", blockIdx.x, threadIdx.x);
 }
 
 int main(){
-
-  hello<<<FIXME>>>();
-  cudaDeviceSynchronize();
+  hello<<<2, 2>>>();
+  gpuErrchk( cudaPeekAtLastError() );
+  
+  gpuErrchk(cudaDeviceSynchronize());
 }
 
